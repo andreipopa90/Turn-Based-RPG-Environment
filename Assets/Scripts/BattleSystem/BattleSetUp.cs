7052f560099e4e08eb0fe880f54bcbb84358fba0
Andrei Popa <popa.andrey90@yahoo.com>
2023-01-11 16:27:52 +0100

    small update

--- Assets/Scripts/BattleSystem/BattleSetUp.cs
+++ Assets/Scripts/BattleSystem/BattleSetUp.cs
@@ -81,7 +81,7 @@ namespace BattleSystem
                 return character;
             }
 
-            characterUnit.Level = GameState.CurrentLevel + 10;
+            characterUnit.Level = GameState.CurrentLevel + 5;
             characterUnit.SetTypes(GameState.StarterStats.Types);
             characterUnit.Manager = Manager;
             characterUnit.OriginalStats = GameState.StarterStats;
